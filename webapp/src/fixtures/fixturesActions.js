238f093ad379be4463333aa864475b4ddfa94d7f
vgutta <vineethg@udel.edu>
2018-06-28 20:28:54 -0400

    fixtures

---
+++ webapp/src/fixtures/fixturesActions.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export function fetchFixtures() {
+    return (dispatch) => {
+        dispatch({type: 'FETCH_FIXTURES'});
+        axios.get("https://raw.githubusercontent.com/openfootball/world-cup.json/master/2018/worldcup.json",{})
+            .then(response => {
+                dispatch({type: "FETCH_FIXTURES_FULLFILLED", payload: response.data});
+            })
+            .catch(err => {
+                dispatch({type: "FETCH_FIXTURES_REJECTED", payload: response.err});
+            });
+    }
+}
